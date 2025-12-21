87a7f46dd3a2ff1f6f18149f79afd99e765e530d
byufdhs2219 <byufd171@gmail.com>
2025-12-21 14:12:58 +0700

    app3

--- app/app.js
+++ app/app.js
@@ -19,6 +19,9 @@ app.get('/app1', (req, res) => {
 app.get('/app2', (req, res) => {
   res.send('Hello this App 2!')
 });
+app.get('/app3', (req, res) => {
+  res.send('Hello this App 3!')
+});
 
 app.get('/users', (req, res, next) => {
   const sql = "SELECT * FROM tb_data ORDER BY id desc"
